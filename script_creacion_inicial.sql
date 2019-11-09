0a53954dd356c119e6a0aa665dc7f8bbaccc15cc
ndeberuti <ndeberuti@gmail.com>
2019-11-09 10:55:01 -0300

    Cambio de nombre de tablas a plural

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -7,24 +7,24 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE SCHEMA [NO_SRTA_E_GATOREI] AUTHORIZATION [gdCupon2019]
 GO
-create table [NO_SRTA_E_GATOREI].[PERMISO](
+create table [NO_SRTA_E_GATOREI].[PERMISOS](
 	[PERMISO_ID] int IDENTITY(1,1) primary key,
 	[PERMISO_DESC] [nvarchar](255) not null,
 	[PERMISO_CLAVE] [nvarchar](255) not null
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[ROL](
+create table [NO_SRTA_E_GATOREI].[ROLES](
 		[ROL_ID] int IDENTITY(1,1) primary key,
 		[NOMBRE] [nvarchar](100) not null,
 		[BAJA_LOGICA] [bit] not null default 0
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[PERMISO_ROL](
+create table [NO_SRTA_E_GATOREI].[PERMISOS_ROLES](
 	[ROL_ID] int foreign KEY REFERENCES [NO_SRTA_E_GATOREI].[ROL](ROL_ID) not null,
 	[PERMISO_ID] int foreign KEY REFERENCES [NO_SRTA_E_GATOREI].[PERMISO](PERMISO_ID) not null
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[USUARIO](
+create table [NO_SRTA_E_GATOREI].[USUARIOS](
 	[USUARIO_ID] int IDENTITY(1,1) primary key,
 	[USERNAME] [nvarchar](250) unique not null,
 	[PASSWORD] [nvarchar](250) not null,
@@ -34,19 +34,19 @@ create table [NO_SRTA_E_GATOREI].[USUARIO](
 	[LOGIN_FALLIDO] int not null default 0
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[USUARIO_ROL](
+create table [NO_SRTA_E_GATOREI].[USUARIOS_ROLES](
 	[ROL_ID] int foreign KEY REFERENCES [NO_SRTA_E_GATOREI].[ROL](ROL_ID) not null,
 	[USUARIO_ID] int foreign KEY REFERENCES [NO_SRTA_E_GATOREI].[USUARIO](USUARIO_ID) not null
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[DIRECCION](
+create table [NO_SRTA_E_GATOREI].[DIRECCIONES](
 	[DIRECCION_ID] int IDENTITY(1,1) primary key,
 	[DIRECCION] [nvarchar](255) not null,
 	[CIUDAD] [nvarchar](255) not null,
 	[CODIGO_POSTAL] [numeric](4,0) default 0	
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[CLIENTE](
+create table [NO_SRTA_E_GATOREI].[CLIENTES](
 	[CLIENTE_ID] int IDENTITY(1,1) primary key,
 	[DNI] [numeric](18, 0) unique not null,
 	[NOMBRE] [nvarchar](255) not null,
@@ -58,7 +58,7 @@ create table [NO_SRTA_E_GATOREI].[CLIENTE](
 	[DIRECCION_ID] int not null foreign key REFERENCES [NO_SRTA_E_GATOREI].[DIRECCION](DIRECCION_ID)
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[PROVEEDOR](
+create table [NO_SRTA_E_GATOREI].[PROVEEDORES](
 	[PROVEEDOR_ID] int IDENTITY(1,1) primary key,
 	[CUIT] [nvarchar](20) unique not null,
 	[RAZON_SOCIAL] [nvarchar](255) unique not null,
@@ -70,13 +70,13 @@ create table [NO_SRTA_E_GATOREI].[PROVEEDOR](
 	[DIRECCION_ID] int not null foreign key REFERENCES [NO_SRTA_E_GATOREI].[DIRECCION](DIRECCION_ID)
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[TARJETA](
+create table [NO_SRTA_E_GATOREI].[TARJETAS](
 	[TARJETA_ID] int IDENTITY(1,1) primary key,
 	[TITULAR] [nvarchar](255) not null,
 	[FECHA_VENCIMIENTO] [nvarchar](5) not null
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[CREDITO](
+create table [NO_SRTA_E_GATOREI].[CREDITOS](
 	[CREDITO_ID] int IDENTITY(1,1) primary key,
 	[FECHA] datetime not null,
 	[TIPO_PAGO] [nvarchar](100) not null,
@@ -85,7 +85,7 @@ create table [NO_SRTA_E_GATOREI].[CREDITO](
 	[TARJETA_ID] int not null foreign key REFERENCES [NO_SRTA_E_GATOREI].[TARJETA](TARJETA_ID)
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[OFERTA](
+create table [NO_SRTA_E_GATOREI].[OFERTAS](
 	[OFERTA_ID] int IDENTITY(1,1) primary key,
 	[PRECIO_LISTA] [numeric](18, 2) NULL,
 	[PRECIO_OFERTA] [numeric](18, 2) NULL,
@@ -98,26 +98,26 @@ create table [NO_SRTA_E_GATOREI].[OFERTA](
 	[ENTREGADO] [datetime]
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[CONSUMO_CUPON](
+create table [NO_SRTA_E_GATOREI].[CONSUMOS_CUPONES](
 	[CONSUMO_CUPON_ID] int IDENTITY(1,1) primary key,
 	[OFERTA_ID] int not null foreign key REFERENCES [NO_SRTA_E_GATOREI].[OFERTA](OFERTA_ID),
 	[CLIENTE_ID] int not null foreign key REFERENCES [NO_SRTA_E_GATOREI].[CLIENTE](CLIENTE_ID),
 	[FECHA_CONSUMO] [datetime] not null
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[COMPRA](
+create table [NO_SRTA_E_GATOREI].[COMPRAS](
 	[COMPRA_ID] int IDENTITY(1,1) primary key,
 	[OFERTA_ID] int not null foreign key REFERENCES [NO_SRTA_E_GATOREI].[OFERTA](OFERTA_ID),
 	[CLIENTE_ID] int not null foreign key REFERENCES [NO_SRTA_E_GATOREI].[CLIENTE](CLIENTE_ID),
 	[FECHA_COMPRA] [datetime] not null
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[FACTURA](
+create table [NO_SRTA_E_GATOREI].[FACTURAS](
 	[FACTURA_ID] int IDENTITY(1,1) primary key,
 	[IMPORTE] [numeric](18,0)
 ) ON [PRIMARY]
 GO
-create table [NO_SRTA_E_GATOREI].[FACTURA_COMPRA](
+create table [NO_SRTA_E_GATOREI].[FACTURAS_COMPRAS](
 	[FACTURA_ID] int not null foreign key REFERENCES [NO_SRTA_E_GATOREI].[FACTURA](FACTURA_ID),
 	[COMPRA_ID] int not null foreign key REFERENCES [NO_SRTA_E_GATOREI].[COMPRA](COMPRA_ID)
 ) ON [PRIMARY] 
