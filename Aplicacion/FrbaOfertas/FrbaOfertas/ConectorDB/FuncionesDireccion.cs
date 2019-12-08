b0a2272c41f57528dce3d0fbf15fecb53f8090f4
matias <landaumatias@gmail.com>
2019-12-08 14:59:33 -0800

    sacado del git ignore

---
+++ Aplicacion/FrbaOfertas/FrbaOfertas/ConectorDB/FuncionesDireccion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrbaOfertas.Modelo.Roles;
+using System.Data.SqlClient;
+using FrbaOfertas.BaseDeDatos;
+
+namespace FrbaOfertas.ConectorDB
+{
+    class FuncionesDireccion
+    {
+        public static Direccion extractDireccion(SqlDataReader reader)
+        { 
+            return new Direccion(reader.GetInt16(reader.GetOrdinal("DIRECCION_ID")),reader.GetString(reader.GetOrdinal("CIUDAD")), reader.GetString(reader.GetOrdinal("CALLE")),reader.GetString(reader.GetOrdinal("CODIGO_POSTAL")));
+        }
+
+        public static void actualizarDireccion(Direccion direccion)
+        {
+            SqlConnection con = new SqlConnection(Conexion.getStringConnection());
+            con.Open();
+
+            string sql = "UPDATE [NO_SRTA_E_GATOREI].[DIRECCIONES] ";
+            sql += "SET [DIRECCION] = @DIRECCION ";
+            sql += ",[CIUDAD] = @CIUDAD ";
+            sql += ",[CODIGO_POSTAL] = @CP ";
+            sql += "WHERE DIRECCION_ID = @ID ";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            cmd.Parameters.Add("@DIRECCION",direccion.Calle;
+            cmd.Parameters.Add("@CIUDAD",direccion.Ciudad);
+            cmd.Parameters.Add("@CP",direccion.codigoPostal);
+            cmd.Parameters.Add("@ID",direccion.id);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+    }
+}
---
+++ Aplicacion/FrbaOfertas/FrbaOfertas/Modelo/Roles/Direccion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrbaOfertas.Modelo.Roles
+{
+    class Direccion
+    {
+        public int id { get; set; }
+        public string Ciudad { get; set; }
+        public string Calle { get; set; }
+        public string codigoPostal { get; set; }
+        public string Piso { get; set; }
+        public string Dpto { get; set; }
+        public string Localidad { get; set; }
+
+        public Direccion(int id, string ciudad, string calle, string codigoPostal)
+        {
+            this.id = id;
+            this.Ciudad = ciudad;
+            this.Calle = calle;
+            this.codigoPostal = codigoPostal;
+        }
+    }
+}
