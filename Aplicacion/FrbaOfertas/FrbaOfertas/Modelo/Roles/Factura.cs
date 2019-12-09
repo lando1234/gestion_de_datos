6e0912a42174000f08451f4a65974c9b8831bb63
matias <landaumatias@gmail.com>
2019-12-08 20:27:27 -0800

    facturas

--- Aplicacion/FrbaOfertas/FrbaOfertas/Modelo/Roles/Factura.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/Modelo/Roles/Factura.cs
@@ -8,5 +8,15 @@ namespace FrbaOfertas.Modelo.Roles
 {
     class Factura
     {
+        public int id { get; set; }
+        public Int64 numero { get; set; }
+        public DateTime fecha { get; set; }
+        public double importe { get; set; }
+
+        public Factura(Int64 numero, double importe)
+        {
+            this.numero = numero;
+            this.importe = importe;
+        }
     }
 }
