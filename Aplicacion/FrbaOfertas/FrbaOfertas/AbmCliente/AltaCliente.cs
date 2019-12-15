ea820a56ebf30852183347d9ecc1307687fe3350
Nicolás de Beruti <ndeberuti@gmail.com>
2019-12-15 02:42:31 -0300

    revisarAltaCliente

--- Aplicacion/FrbaOfertas/FrbaOfertas/AbmCliente/AltaCliente.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/AbmCliente/AltaCliente.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FrbaOfertas.Utils;
 
 namespace FrbaOfertas.AbmCliente
 {
@@ -29,10 +30,41 @@ namespace FrbaOfertas.AbmCliente
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Direccion direccion = new Direccion(null, txt_ciudad.Text, txt_calle.Text, Int16.Parse(txt_cpostal.Text));
-            Cliente cliente = new Cliente(null, int.Parse(txt_dni.Text), txt_nombre.Text, txt_apellido.Text, txt_mail.Text, int.Parse(txt_tel.Text), dateTimePicker.Value,true, null, direccion);
-            ConectorDB.FuncionesCliente.altaCliente(cliente, usuario);
-        }
+            
+            if (Validador.isNumeric(txt_cpostal.Text))
+            {
+
+                //0sageBox.Show("Codigo postal debe ser númerico", "PC NOT NUMERIC", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw new System.ArgumentException("Codigo postal debe ser númerico", "original");
+                
+            }
+            else if (Validador.isNumeric(txt_dni.Text))
+            {
+                MessageBox.Show("dni debe ser númerico", "DNI NOT NUMERIC", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }   
+            else if (Validador.IsValidEmail(txt_mail.Text))
+            {
+                MessageBox.Show("el mail debe ser válido", "EMAIL NOT VALID", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+            else if (Validador.IsValidEmail(txt_tel.Text))
+            {
+                MessageBox.Show("el telefono debe ser númerico", "TEL NOT VALID", MessageBoxButtons.OK, MessageBoxIcon.Error);
+               
+            }
+            else
+            {
+
+                Direccion direccion = new Direccion(null, txt_ciudad.Text, txt_calle.Text, Int16.Parse(txt_cpostal.Text));
+                Cliente cliente = new Cliente(null, int.Parse(txt_dni.Text), txt_nombre.Text, txt_apellido.Text, txt_mail.Text, int.Parse(txt_tel.Text), dateTimePicker.Value, true, null, direccion);
+                ConectorDB.FuncionesCliente.altaCliente(cliente, usuario);
+              
+            }
+
+     
+    }
+
 
     }
+    
 }
