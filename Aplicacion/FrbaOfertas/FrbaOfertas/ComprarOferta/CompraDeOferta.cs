a39ba547d861021d0da0f93cffc8ab1df31627c0
kevinvarela <kevenvarela@gmail.com>
2019-12-15 22:44:20 -0300

    terminado consumir cupon

--- Aplicacion/FrbaOfertas/FrbaOfertas/ComprarOferta/CompraDeOferta.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/ComprarOferta/CompraDeOferta.cs
@@ -14,43 +14,54 @@ namespace FrbaOfertas.ComprarOferta
 {
     public partial class CompraDeOferta : Form
     {
-        List<Oferta> ofertas = new List<Oferta>();
+
+        List<Oferta> ofertas;
         public CompraDeOferta()
         {
             InitializeComponent();
         }
 
         private void dataGridCompraOfertas_Load(object sender, DataGridViewCellEventArgs e)
-        {
-
-          
-        }
+        {}
 
         private void dataGridCompraOfertas_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            if (e.ColumnIndex == dataGridCompraOfertas.Columns["Comprar"].Index && (dataGridCompraOfertas.Rows.Count > 1) && e.RowIndex != dataGridCompraOfertas.Rows.Count - 1) {
+            int ofertaAComprar = (int)dataGridCompraOfertas.Rows[e.RowIndex].Cells[0].Value;
+            Oferta ofertaObjeto = this.ofertas.Find(a => a.id.Equals(ofertaAComprar));
 
-                Oferta oferta = new Oferta();
-               int idProveedor = Int32.Parse(dataGridCompraOfertas.Rows[e.RowIndex].Cells["Proveedor"].Value.ToString());
-
-
-               FrbaOfertas.ComprarOferta.Form1 dialog = new FrbaOfertas.ComprarOferta.Form1(oferta);
-               dialog.ShowDialog(this);
-            }
+            this.compraOferta(ofertaObjeto);
         }
 
         private void CompraDeOferta_Load(object sender, EventArgs e)
         {
           
-            
-            List<Oferta> ofertas = FuncionesOferta.getOfertasNoVencidas();
-            //ACA SE AGREGAN LAS OFERTAS AL DATA GRID PARA MOSTRARSE
+            ofertas = FuncionesOferta.getOfertasNoVencidas();
             foreach (Oferta oferta in ofertas)
             {
                 Object[] row = new Object[] { oferta.proveedor_id, oferta.descripcion, oferta.precio_oferta, oferta.cantidad, oferta.maximo_usuario };
                 dataGridCompraOfertas.Rows.Add(row);
             }
 
+        }
+
+        private void compraOferta(Oferta oferta) {
+            int idCliente = ConectorDB.FuncionesCliente.getClienteLogueado();
+
+            Cliente cliente = ConectorDB.FuncionesCliente.traerCliente(idCliente);
+
+            decimal codigoCupon = ConectorDB.FuncionesOferta.comprarOferta(oferta, cliente);
+
+            if (codigoCupon == -1) {
+                MessageBox.Show("Alcanzo su limite compra", "Alcanzo su limite compra", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (codigoCupon == -2)
+            {
+                MessageBox.Show("No tiene credito suficiente", "No tiene credito suficiente", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else {
+                MessageBox.Show("CODIGO CUPON: " + codigoCupon.ToString(), "Cupon comprado correctamente", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
         }
 
         
