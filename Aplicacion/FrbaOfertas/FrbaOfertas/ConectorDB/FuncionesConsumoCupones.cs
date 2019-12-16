673145f008b342af4ceb6201425973baacd97288
Nicolás de Beruti <ndeberuti@gmail.com>
2019-12-15 22:33:40 -0300

    ObtenerCuponNOVencido

--- Aplicacion/FrbaOfertas/FrbaOfertas/ConectorDB/FuncionesConsumoCupones.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/ConectorDB/FuncionesConsumoCupones.cs
@@ -1,4 +1,5 @@
 ﻿using FrbaOfertas.BaseDeDatos;
+using FrbaOfertas.Modelo.Roles;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -10,6 +11,39 @@ namespace FrbaOfertas.ConectorDB
 {
     class FuncionesConsumoCupones
     {
+
+        public static List<Cupon> obtenerCuponPorIdNoVencido(int idCliente) {
+            List<Cupon> cupones = new List<Cupon>();
+
+        
+            SqlConnection con = new SqlConnection(Conexion.getStringConnection());
+            con.Open();
+            string sql = "SELECT * FROM [NO_SRTA_E_GATOREI].CUPONES WHERE CLIENTE_ID = @ID_CLIENTE AND  @FECHA_SISTEMA < FECHA_CONSUMO";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@FECHA_SISTEMA", Utils.HoraSistema.get());
+            cmd.Parameters.AddWithValue("@ID_CLIENTE", idCliente);
+
+        
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read()) {
+
+                Cupon c = new Cupon(
+                    reader.GetInt32(reader.GetOrdinal("CUPON_ID")),
+                    reader.GetInt32(reader.GetOrdinal("CLIENTE_ID")),
+                    reader.GetDateTime(reader.GetOrdinal("FECHA_CONSUMO"))
+                );
+                cupones.Add(c);
+            }
+           
+            con.Close();
+           
+           
+        
+            return cupones;
+        
+        }
+        
         //public static void consumirCupon(Cupon cupon)
         //{
         //    SqlConnection con = new SqlConnection(Conexion.getStringConnection());
---
+++ Aplicacion/FrbaOfertas/FrbaOfertas/Modelo/Roles/Cupon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrbaOfertas.Modelo.Roles
+{
+    public class Cupon
+    {
+        int id;
+        int clienteId;
+        DateTime fechaConsumo;
+
+        public Cupon(int id, int clienteId, DateTime fechaConsumo) {
+            this.id = id;
+            this.clienteId = clienteId;
+            this.fechaConsumo = fechaConsumo;
+        } 
+    }
+}
